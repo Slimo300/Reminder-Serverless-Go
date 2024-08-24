ab16f9be85791183f282f2fddde8fedae976e365
Slimo300 <sebastian.slimak707@gmail.com>
2024-08-24 19:15:52 +0200

    lambdas modified to use pkg

--- lambdas/alarm-executor/main.go
+++ lambdas/alarm-executor/main.go
@@ -2,50 +2,23 @@ package main
 
 import (
 	"context"
-	"log"
-	"os"
 
+	alarmexecutor "github.com/Slimo300/Reminder-Serverless-Go/pkg/handlers/alarm-executor"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
-	"github.com/aws/aws-sdk-go-v2/service/sns/types"
 )
-
-var snsClient *sns.Client
-
-type alarmEvent struct {
-	UserID  string `json:"userID"`
-	Message string `json:"message"`
-}
-
-func Handler(event alarmEvent) error {
-
-	if _, err := snsClient.Publish(context.Background(), &sns.PublishInput{
-		TopicArn: aws.String(os.Getenv("SNS_TOPIC_ARN")),
-		Message:  &event.Message,
-		MessageAttributes: map[string]types.MessageAttributeValue{
-			"userID": {
-				DataType:    aws.String("String"),
-				StringValue: &event.UserID,
-			},
-		},
-	}); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func main() {
 
 	cfg, err := config.LoadDefaultConfig(context.Background())
 	if err != nil {
-		log.Println(err)
 		return
 	}
 
-	snsClient = sns.NewFromConfig(cfg)
+	handler := alarmexecutor.Handler{
+		SNSClient: sns.NewFromConfig(cfg),
+	}
 
-	lambda.Start(Handler)
+	lambda.Start(handler.Handle)
 }
